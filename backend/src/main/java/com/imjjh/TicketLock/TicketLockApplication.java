9402313ed50d8efdd18b3d2ae4e607c4d3bc5ff2
jangjunho2 <rldirwkd328@naver.com>
2025-11-30 13:31:23 +0900

    init backend and mysql(docker)

---
+++ backend/src/main/java/com/imjjh/TicketLock/TicketLockApplication.java
@@ -0,0 +1,13 @@
+package com.imjjh.TicketLock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketLockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketLockApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/imjjh/TicketLock/TicketLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imjjh.TicketLock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketLockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
