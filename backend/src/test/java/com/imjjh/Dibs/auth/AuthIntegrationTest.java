b823f9817b2e21a7b5085824973d58648b9314eb
imjjh <rldirwkd328@naver.com>
2026-01-27 17:13:46 +0900

    test(backend): 회원가입 통합 테스트 추가

---
+++ backend/src/test/java/com/imjjh/Dibs/auth/AuthIntegrationTest.java
@@ -0,0 +1,40 @@
+package com.imjjh.Dibs.auth;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.imjjh.Dibs.auth.dto.RegisterRequestDto;
+import com.imjjh.Dibs.auth.service.AuthService;
+import com.imjjh.Dibs.auth.user.repository.UserRepository;
+import com.imjjh.common.annotation.IntegrationTest;
+
+@IntegrationTest
+public class AuthIntegrationTest {
+
+    @Autowired
+    private AuthService authService;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("회원가입 통합 테스트")
+    void 회원가입_통합_테스트() {
+        // given
+        RegisterRequestDto dto = RegisterRequestDto.builder()
+                .username("admin")
+                .password("Password123!@")
+                .nickname("imadmin")
+                .email("admin123@dibs.com")
+                .build();
+
+        // when
+        authService.register(dto);
+
+        // then
+        assertThat(userRepository.findByUsername("admin")).isPresent();
+    }
+
+}
