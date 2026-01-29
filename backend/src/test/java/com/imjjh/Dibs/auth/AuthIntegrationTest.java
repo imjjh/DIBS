5e65221f1e3e008f3dde01790e7ae4b44162544a
imjjh <rldirwkd328@naver.com>
2026-01-29 13:55:26 +0900

    fix(test): 통합테스트시 S3 서비스를 Mock으로 대체하여 외부 의존성 제거

--- backend/src/test/java/com/imjjh/Dibs/auth/AuthIntegrationTest.java
+++ backend/src/test/java/com/imjjh/Dibs/auth/AuthIntegrationTest.java
@@ -5,10 +5,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import com.imjjh.Dibs.auth.dto.RegisterRequestDto;
 import com.imjjh.Dibs.auth.service.AuthService;
 import com.imjjh.Dibs.auth.user.repository.UserRepository;
+import com.imjjh.Dibs.common.service.S3Service;
 import com.imjjh.common.annotation.IntegrationTest;
 
 @IntegrationTest
@@ -18,6 +20,9 @@ public class AuthIntegrationTest {
     private AuthService authService;
     @Autowired
     private UserRepository userRepository;
+
+    @MockitoBean
+    private S3Service s3Service;
 
     @Test
     @DisplayName("회원가입 통합 테스트")
